02af22e5aab74323fa3bdf64481d73e3f53225cd
Igor Baryshev <lo.2s@yandex.ru>
2019-07-19 13:26:09 +0300

    Добавление перечисления с типом пользователя

---
+++ src/Enums/ZoomUserType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MacsiDigital\Zoom\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class ZoomUserType extends Enum
+{
+    const TYPE_BASIC = 1;
+    const TYPE_PRO = 2;
+    const TYPE_CORP = 3;
+}
