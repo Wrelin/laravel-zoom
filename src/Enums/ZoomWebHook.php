b9c4c3d924ec21a4909e0e543788981307d32e27
Alex <aleksandr.garanov@maximumtest.ru>
2019-08-01 12:03:14 +0300

    Добавление типа вебхука для завершения записи

--- src/Enums/ZoomWebHook.php
+++ src/Enums/ZoomWebHook.php
@@ -9,4 +9,5 @@ final class ZoomWebHook extends Enum
     const EVENT_MEETING_CREATED = 'meeting.created';
     const EVENT_MEETING_STARTED = 'meeting.started';
     const EVENT_MEETING_ENDED = 'meeting.ended';
+    const EVENT_RECORDING_COMPLETED = 'recording.completed';
 }
